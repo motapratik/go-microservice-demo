f06c9c777f421b34398f7bd52dcbbdd9aeb9761f
Pratik Mota <mota.pratik@gmail.com>
2022-11-21 00:37:53 +0530

    handler for broker service

--- broker-service/cmd/api/routes.go
+++ broker-service/cmd/api/routes.go
@@ -1,4 +1,4 @@
-package mmain
+package main
 
 import (
 	"net/http"
@@ -7,20 +7,21 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
 )
-func routes() http.Handler{
+func (app *Config) routes() http.Handler{
 	mux := chi.NewRouter()
 
 	// Specify who is allowed to connect
 	mux.Use(cors.Handler(cors.Options{
 		AllowedOrigins: []string{"https://*", "http://*"},
 		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTION"},
-		AllowedHeders: []string{"Accept", "Authorization","Content-Type", "X-CSRF-Token"},
+		AllowedHeaders: []string{"Accept", "Authorization","Content-Type", "X-CSRF-Token"},
 		ExposedHeaders: []string{"Link"},
-		AllowedCredentials: true,
+		AllowCredentials: true,
 		MaxAge: 300,
 	}))
 
 	mux.Use(middleware.Heartbeat("/ping"))
+	mux.Post("/", app.Broker)
 
 	return mux
 }
